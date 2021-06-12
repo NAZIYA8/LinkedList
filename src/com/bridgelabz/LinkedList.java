2c632703dc2e6512cf7d45c720eafac6890be9a1
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-12 22:30:28 +0530

    Created a Simple Linked List

---
+++ src/com/bridgelabz/LinkedList.java
@@ -0,0 +1,38 @@
+package com.bridgelabz;
+
+public class LinkedList<T> {
+    Node<T> head;
+
+    /**
+     * Appends elements at the end of the list
+     *
+     * @param data elements passed
+     */
+    public void add(T data) {
+        Node<T> newNode = new Node<>();
+        newNode.data = data;
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> node = head;
+            while (node.next != null) {
+                node = node.next;
+            }
+            node.next = newNode;
+        }
+    }
+
+    /**
+     * This method prints all the elements
+     * Here Traverse till the null and
+     * print till the last node's next element is null
+     */
+    public void show() {
+        Node<T> node = head;
+        while (node.next != null) {
+            System.out.println(node.data);
+            node = node.next;
+        }
+        System.out.println(node.data);
+    }
+}
--- src/com/bridgelabz/Runner.java
+++ src/com/bridgelabz/Runner.java
@@ -2,6 +2,11 @@ package com.bridgelabz;
 
 public class Runner {
     public static void main(String[] args) {
-        System.out.println("Welcome to Linked List Implementation Problems");
+        LinkedList<Integer> numberList = new LinkedList<>();
+        numberList.add(56);
+        numberList.add(30);
+        numberList.add(70);
+        numberList.show();
+
     }
 }
